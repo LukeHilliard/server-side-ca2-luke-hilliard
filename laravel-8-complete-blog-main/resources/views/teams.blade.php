62fc52100a5019ebea3fd8c8ce00e568c00194ac
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-03-15 19:16:04 +0000

    First card added, the rest should follow this format

--- laravel-8-complete-blog-main/resources/views/teams.blade.php
+++ laravel-8-complete-blog-main/resources/views/teams.blade.php
@@ -8,7 +8,24 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
         <!-- Team 1 -->
         <div class="bg-gray-100 p-4 rounded-md">
-
+            <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
+                    <img src="https://media.formula1.com/content/dam/fom-website/2018-redesign-assets/team%20logos/mercedes.jpg"/>
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        Mercedes-AMG Petronas Formula One Team
+                    </h5>
+                    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        Mercedes-AMG Petronas Formula One Team, also known as Mercedes, is a German-British Formula One racing team and constructor. They are one of the most successful teams in Formula One history, winning numerous Drivers' and Constructors' Championships.
+                    </p>
+                </div>
+                <div class="p-6 pt-0">
+                    <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Read More
+                    </button>
+                </div>
+            </div>
         </div>
 
         <!-- Team 2 -->
