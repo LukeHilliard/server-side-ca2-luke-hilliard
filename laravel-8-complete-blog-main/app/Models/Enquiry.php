37fd6a63a8e174aee59bd4653608e1528a4e28a7
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-03-15 17:47:57 +0000

    Updated enquiry modal

--- laravel-8-complete-blog-main/app/Models/Enquiry.php
+++ laravel-8-complete-blog-main/app/Models/Enquiry.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Enquiry extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'email', 'message', 'is_urgent', 'created_at', 'updated_at', 'first_name', 'last_name'];
 }
