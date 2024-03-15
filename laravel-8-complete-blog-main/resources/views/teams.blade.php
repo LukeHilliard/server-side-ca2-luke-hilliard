b61f675433888b06f08c44f97dce910f931d01e9
LukeHilliard <119594459+LukeHilliard@users.noreply.github.com>
2024-03-15 19:40:39 +0000

    Final teams added

--- laravel-8-complete-blog-main/resources/views/teams.blade.php
+++ laravel-8-complete-blog-main/resources/views/teams.blade.php
@@ -17,8 +17,7 @@
                         Mercedes-AMG Petronas Formula One Team
                     </h5>
                     <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-                        Mercedes-AMG Petronas Formula One Team, also known as Mercedes, is a German-British Formula One racing team and constructor. They are one of the most successful teams in Formula One history, winning numerous Drivers' and Constructors' Championships.
-                    </p>
+                        A dominant force in Formula One, Mercedes has secured multiple championships, boasting exceptional engineering and strategic prowess.                    </p>
                 </div>
                 <div class="p-6 pt-0">
                     <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
@@ -31,23 +30,22 @@
         <!-- Team 2 -->
         <div class="bg-gray-100 p-4 rounded-md">
             <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-            <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
-                <img src="https://media.formula1.com/content/dam/fom-website/2018-redesign-assets/team%20logos/red%20bull.jpg"/>
+                <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
+                    <img src="https://media.formula1.com/content/dam/fom-website/2018-redesign-assets/team%20logos/red%20bull.jpg"/>
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        Red Bull Racing Honda
+                    </h5>
+                    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        Known for its aggressive racing style, Red Bull Racing has emerged as a top contender, challenging traditional powerhouses with its innovative approach.                </p>
+                </div>
+                <div class="p-6 pt-0">
+                    <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Read More
+                    </button>
+                </div>
             </div>
-            <div class="p-6">
-                <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                    >Red Bull Racing Honda
-                </h5>
-                <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-                    Red Bull Racing Honda, also known as Red Bull Racing, is an Austrian Formula One racing team and constructor. Founded by the energy drink company Red Bull GmbH, the team has enjoyed significant success in Formula One, particularly in the 2010s.
-                </p>
-            </div>
-            <div class="p-6 pt-0">
-                <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                    Read More
-                </button>
-            </div>
-        </div>
         </div>
 
         <!-- Team 3 -->
@@ -61,8 +59,7 @@
                         Scuderia Ferrari
                     </h5>
                     <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-                        Scuderia Ferrari, commonly referred to as Ferrari, is the oldest and most successful team in Formula One history. Founded in 1929 by Enzo Ferrari, the Italian racing team has a storied legacy in the sport, boasting a record number of Constructors' Championships and Drivers' Championships.
-                    </p>
+                        With a rich heritage and passionate fan base, Ferrari commands respect in Formula One, striving for excellence and glory on the track.                    </p>
                 </div>
                 <div class="p-6 pt-0">
                     <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
@@ -83,8 +80,7 @@
                         McLaren F1 Team
                     </h5>
                     <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
-                        McLaren F1 Team, known simply as McLaren, is one of the most iconic and successful teams in Formula One history. Founded by Bruce McLaren in 1963, the British racing team has achieved numerous victories and championships over the years. With a rich heritage of innovation and engineering excellence, McLaren continues to push the boundaries of technology and performance in Formula One.
-                    </p>
+                        McLaren's legacy in Formula One is defined by innovation and success, pushing boundaries with cutting-edge technology and driving performance.                    </p>
                 </div>
                 <div class="p-6 pt-0">
                     <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
@@ -96,32 +92,128 @@
 
         <!-- Team 5 -->
         <div class="bg-gray-100 p-4 rounded-md">
-
+            <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
+                    <img src="https://www.rtlnieuws.nl/sites/default/files/styles/liggend/public/content/images/2019/12/19/191219_AlphaTauri.jpg?itok=UoQ8vdtO"/>
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        Scuderia AlphaTauri Honda
+                    </h5>
+                    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        Rising swiftly in the ranks, AlphaTauri showcases Italian engineering prowess, making significant strides as a competitive force in Formula One.                    </p>
+                </div>
+                <div class="p-6 pt-0">
+                    <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Read More
+                    </button>
+                </div>
+            </div>
         </div>
 
         <!-- Team 6 -->
         <div class="bg-gray-100 p-4 rounded-md">
-
+            <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
+                    <img src="https://media.formula1.com/content/dam/fom-website/2018-redesign-assets/team%20logos/aston%20martin%202024.jpg"/>
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        Aston Martin Cognizant Formula One Team
+                    </h5>
+                    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        Rebranded from Racing Point, Aston Martin returns to Formula One, combining British heritage with the ambition to challenge for top honors.               </p>
+                </div>
+                <div class="p-6 pt-0">
+                    <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Read More
+                    </button>
+                </div>
+            </div>
         </div>
 
         <!-- Team 7 -->
         <div class="bg-gray-100 p-4 rounded-md">
-
+            <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
+                    <img src="https://media.formula1.com/content/dam/fom-website/2018-redesign-assets/team%20logos/alpine.jpg"/>
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        Alpine F1 Team
+                    </h5>
+                    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        Formerly Renault, Alpine represents French automotive excellence, aiming to translate its motorsport heritage into Formula One success.               </p>
+                </div>
+                <div class="p-6 pt-0">
+                    <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Read More
+                    </button>
+                </div>
+            </div>
         </div>
 
         <!-- Team 8 -->
         <div class="bg-gray-100 p-4 rounded-md">
-
+            <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
+                    <img src="https://media.formula1.com/image/upload/content/dam/fom-website/manual/teams/Sauber/Alfa_Romeo_Racing_logo.jpg"/>
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        Alfa Romeo Racing Orlen
+                    </h5>
+                    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        Embodying Italian elegance, Alfa Romeo Racing brings a blend of tradition and innovation to Formula One, seeking to make its mark on the grid.               </p>
+                </div>
+                <div class="p-6 pt-0">
+                    <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Read More
+                    </button>
+                </div>
+            </div>
         </div>
 
         <!-- Team 9 -->
         <div class="bg-gray-100 p-4 rounded-md">
-
+            <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
+                    <img src="https://media.formula1.com/content/dam/fom-website/2018-redesign-assets/team%20logos/haas.jpg"/>
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        Haas F1 Team
+                    </h5>
+                    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        An American outfit in Formula One, Haas aspires to compete at the highest level, leveraging partnerships and technology to drive performance.               </p>
+                </div>
+                <div class="p-6 pt-0">
+                    <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Read More
+                    </button>
+                </div>
+            </div>
         </div>
 
         <!-- Team 10 -->
         <div class="bg-gray-100 p-4 rounded-md">
-
+            <div class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-red-500 to-blue-600">
+                    <img src="https://media.formula1.com/content/dam/fom-website/2018-redesign-assets/team%20logos/williams.jpg"/>
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+                        Williams Racing
+                    </h5>
+                    <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+                        With a storied history in Formula One, Williams aims for resurgence, combining experience and determination to reclaim its competitive edge.               </p>
+                </div>
+                <div class="p-6 pt-0">
+                    <button data-ripple-light="true" type="button" class="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Read More
+                    </button>
+                </div>
+            </div>
         </div>
 
     </div>
